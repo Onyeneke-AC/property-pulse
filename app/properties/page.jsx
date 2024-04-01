a19c72513dc0b61c57b1d6f754c55e26d1bb626c
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-04-01 23:42:51 +0100

    latest updates

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,15 +1,20 @@
 import properties from '@/properties.json'
+import PropertyCard from '@/components/PropertyCard';
 
 const PropertiesPage = () => {
 
   return (
     <section class="px-4 py-6">
       <div class="container-xl lg:container m-auto px-4 py-6">
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {properties.map((property) => (
-            <div>{property.name}</div>
-          ))}
-        </div>
+        {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property, index) => (
+              <PropertyCard key={property._id} property={property}/>
+            ))}
+          </div>
+          )}          
       </div>
     </section>
   )
