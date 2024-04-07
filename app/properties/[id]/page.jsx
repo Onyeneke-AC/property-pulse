5c90f2368ad0d9fcef85319615f8b25a5fc29ac2
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-04-07 12:54:23 +0100

    Property Image Display

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import PropertyDetails from "@/components/PropertyDetails";
 import { FaArrowLeft } from 'react-icons/fa'
 import Spinner from "@/components/Spinner";
+import PropertyImages from "@/components/PropertyImages";
 
 const PropertyPage = () => {
   const { id } = useParams();
@@ -130,6 +131,7 @@ const PropertyPage = () => {
         </div>
       </div>
     </section>
+    <PropertyImages images={property.images} />
         </>
       )
     }
