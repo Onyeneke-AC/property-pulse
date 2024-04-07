39605d268c16f16e900aac175c5f99973fd2c5e3
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-04-07 01:04:12 +0100

    Implementing the sign out feature

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -195,7 +195,10 @@ const Navbar = () => {
                         tabIndex="-1"
                         id="user-menu-item-2"
                         >Saved Properties</Link>
-                        <button
+                        <button onClick={() => {
+                          setIsProfileMenuOpen(false);
+                          signOut();
+                        }}
                         className="block px-4 py-2 text-sm text-gray-700"
                         role="menuitem"
                         tabIndex="-1"
