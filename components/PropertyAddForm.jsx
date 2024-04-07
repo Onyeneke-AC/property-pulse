3334288090b00fb5cd1a6c48caa1387738a8bf8a
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-04-07 09:00:05 +0100

    Setting up form input handlers

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -34,9 +34,69 @@ const PropertyAddForm = () => {
         setMounted(true);
     }, []);
 
-    const handleChange = () => { };
-    const handleAmenitiesChange = () => { };
-    const handleImageChange = () => { };
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+
+        // for nested property
+        if (name.includes('.')){
+            const [outerKey, innerKey] = name.split('.')
+        
+
+        setFields((prevFields) => ({
+            ...prevFields,
+            [outerKey]: {
+                ...prevFields[outerKey],
+                [innerKey]: value,
+            }
+        }))
+        } else {
+            // not nested property
+            setFields((prevFields) => ({
+                ...prevFields,
+                [name]: value,
+            }))
+        }
+     };
+    const handleAmenitiesChange = (e) => {
+        const { value, checked } = e.target;
+
+        // clone the current array
+        const updatedAmenities = [...fields.amenities];
+
+        if (checked) {
+            updatedAmenities.push(value);
+        } else {
+            // remove value from array
+            const index = updatedAmenities.indexOf(value);
+
+            if (index !== -1){
+                updatedAmenities.splice(index, 1);
+            }
+        }
+
+        // Update state with updated array
+         setFields((prevFields) => ({
+            ...prevFields,
+            amenities: updatedAmenities,
+         }))
+     };
+    const handleImageChange = (e) => {
+        const { files } = e.target;
+
+        //clone images array
+        const updatedImages = [...fields.images];
+
+        // add new files to the array
+        for (const file of files) {
+            updatedImages.push(file);
+        }
+
+        // update state with array of images
+        setFields((prevFields) => ({
+            ...prevFields,
+            images: updatedImages,
+        }));
+     };
 
   return (
     mounted && (
