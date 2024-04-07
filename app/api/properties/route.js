4a2b43d09ed14a2c4cd267cab0adf502a43e4da9
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-04-07 10:08:43 +0100

    Adding property POST API route

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -18,3 +18,50 @@ export const GET = async (request) => {
     });
   }
 };
+
+// POST /api/properties
+export const POST = async (request) => {
+  try {
+    const formData = await request.formData();
+    
+    // access all values from amenities and images
+    const amenities = formData.getAll('amenities');
+    const images = formData.getAll('images').filter((image) => image.name !== '');
+
+    // create propertyData object for database
+    const propertyData = {
+      type: formData.get('type'),
+      name: formData.get('name'),
+      description: formData.get('description'),
+      location: {
+        street: formData.get('location.street'),
+        city: formData.get('location.city'),
+        state: formData.get('location.state'),
+        zipcode: formData.get('location.zipcode'),
+      },
+      beds: formData.get('beds'),
+      baths: formData.get('baths'),
+      square_feet: formData.get('square_feet'),
+      amenities,
+      rates: {
+        weekly: formData.get('rates.weekly'),
+        monthly: formData.get('rates.monthly'),
+        nightly: formData.get('rates.nightly'),
+      },
+      seller_info: {
+        name: formData.get('seller_info.name'),
+        email: formData.get('seller_info.email'),
+        phone: formData.get('seller_info.phone'),
+      },
+      images,
+    } 
+
+    return new Response(JSON.stringify({message: "success"}), {
+      status: 200,
+    });
+  } catch(err) {
+    return new Response('Failed to add property', {
+      status: 500,
+    });
+  }
+}
