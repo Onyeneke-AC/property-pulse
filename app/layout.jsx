ed4725ebf8f55e702045e59aecd4fbe082403de7
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-04-08 13:07:41 +0100

    Deleting a property

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
 import Navbar from '@/components/Navbar';
-import '@/assets/styles/globals.css';
 import Footer from '@/components/Footer';
 import AuthProvider from '@/components/AuthProvider';
+import { ToastContainer } from 'react-toastify';
+import '@/assets/styles/globals.css';
+import 'react-toastify/dist/ReactToastify.css'
 
 export const metadata = {
     title: 'PropertyPulse | Find the perfect rental',
