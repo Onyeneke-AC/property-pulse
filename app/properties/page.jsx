19011f08677911276fd4ce3fb0c84bc87eccbcb8
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-04-10 13:31:41 +0100

    Display search results

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,5 +1,6 @@
 'use client';
 import PropertyCard from '@/components/PropertyCard';
+import PropertySearchForm from '@/components/PropertySearchForm';
 import Spinner from '@/components/Spinner';
 import { useFetchProperties } from '@/utils/requests';
 
@@ -10,10 +11,17 @@ export default function PropertiesPage() {
   properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
   return (
-    <>
-    {loading && <Spinner loading={loading}/>}
-    {!loading && properties && (
-      <section className="px-4 py-6">
+  <>
+    <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col">
+            <PropertySearchForm />
+        </div>
+    </section>
+    {
+    loading ? (
+        <Spinner loading={loading} />
+    ) : (
+        <section className="px-4 py-6">
         <div className="container-xl lg:container m-auto px-4 py-6">
           { properties.length === 0 ? (
               <p>No properties found</p>
@@ -26,8 +34,9 @@ export default function PropertiesPage() {
             )}          
         </div>
       </section>
-    )}
-    </>
+    )
+    }
+  </>
   )
 }
 
