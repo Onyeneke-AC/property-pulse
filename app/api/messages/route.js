bd1e1c1c785875428646b8fe925197dfc150546b
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-04-11 01:56:05 +0100

    Fetch Messages

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -4,6 +4,35 @@ import { getSessionUser } from "@/utils/getSessionUser";
 
 export const dynamic = 'force-dynamic';
 
+// GET /api/messages
+
+export const GET = async () => {
+    try{
+        await connectDB();
+
+        const sessionUser = await getSessionUser();
+
+        if (!sessionUser || !sessionUser.user) {
+            return new Response('User ID is required', {
+                status: 401,
+            });
+        }
+
+        const { userId } = sessionUser;
+
+        const messages = await Message.find({ recipent: userId })
+            .populate('sender', 'name')
+            .populate('property', 'title')
+
+        return new Response(JSON.stringify(messages), {
+            status: 200,
+        })
+    } catch(err) {
+        console.log(err);
+        return new Response('Something went wrong', { status: 500 });
+    }
+}
+
 // POST /api/messages
 export const POST = async (request) => {
     try{
