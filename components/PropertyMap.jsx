5346ff21e332c2f1883b6afc91db0c571f0f90c5
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-04-13 23:40:04 +0100

    Property map final edit

--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Map, { Marker } from 'react-map-gl';
 import { setDefaults, fromAddress } from "react-geocode";
-import Spinner from "./Spinner";
+import Spinner from "@/components/Spinner";
 import Image from "next/image";
 import pin from '@/assets/images/pin.svg'
 
