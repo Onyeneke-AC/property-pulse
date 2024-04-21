62b1e92f073c5e44f19e5e86fafb507ade8b41e9
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-04-21 22:43:18 +0100

    comment edit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-20 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
-            {/* <!-- Mobile men button--> */}
+            {/* <!-- Mobile menu button--> */}
             <button
               type="button"
               id="mobile-dropdown-button"
