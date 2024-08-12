5e7d5835b57bea3d2070724692c78b0acd24b68f
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-08-12 16:36:24 +0100

    deleting the vercel file

--- context/GlobalContext.js
+++ context/GlobalContext.js
@@ -1,24 +1,26 @@
-'use client';
+"use client";
 import { createContext, useContext, useState } from "react";
 
 // Create context
-const GlobalContext = createContext(); 
+const GlobalContext = createContext();
 
 // Create a provider
-export function GlobalProvider({children}){
-    const [unreadCount, setUnreadCount] = useState(0);
+export function GlobalProvider({ children }) {
+  const [unreadCount, setUnreadCount] = useState(0);
 
-    return (
-        <GlobalContext.Provider value={{
-            unreadCount,
-            setUnreadCount
-        }}>
-            {children}
-        </GlobalContext.Provider>
-    )
+  return (
+    <GlobalContext.Provider
+      value={{
+        unreadCount,
+        setUnreadCount,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 }
 
 // Create a custom hook to access context
 export function useGlobalContext() {
-    return useContext(GlobalContext);
+  return useContext(GlobalContext);
 }
